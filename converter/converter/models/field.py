45665eb325a3a4c602219ff1e1efce228e335f84
Vladislav Klimenko <vladislav.klimenko@lamoda.ru>
2018-10-13 10:24:40 +0300

    Add `field` model

---
+++ converter/converter/models/field.py
@@ -0,0 +1,13 @@
+class Field:
+
+    def __init__(self,
+                 name='',
+                 rname='',
+                 domain='',
+                 description='',
+                 props=[]):
+        self.name = name
+        self.rname = rname
+        self.domain = domain
+        self.description = description
+        self.props = props
