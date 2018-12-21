db632789fef25ce08ef58b9a91564354471167cf
Klimenko Vladislav Sergeevich <vladislav.klimenko@lamoda.ru>
2018-12-21 18:44:36 +0300

    Add PostgreSQL support to main.py

--- converter/converter/main.py
+++ converter/converter/main.py
@@ -3,6 +3,7 @@ import components.loader
 import xml.dom.minidom as md
 
 import dbd_to_xml, xml_to_dbd
+from components import  loader, postgres_builder
 
 import sys
 
@@ -13,7 +14,11 @@ sys.setdefaultencoding('utf8')
 xdb_s = "components/data/prjadm.xdb"
 dbd_r = "result.db"
 xdb_r = "result.xdb"
-pg_name = "db_metadata"
+pg_name = "metadata_db"
 
 xml_to_dbd.parse(xdb_s, dbd_r)
 dbd_to_xml.parse(dbd_r, xdb_r)
+
+schema = loader.Loader(dbd_r).dbd_to_ram()
+postgres_builder = postgres_builder.Postgres_builder(schema)
+postgres_builder.generate_db(pg_name)
