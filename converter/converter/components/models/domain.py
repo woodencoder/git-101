1ee71d4b4f3542b4858f934e93f320c61f0bda61
Klimenko Vladislav Sergeevich <vladislav.klimenko@lamoda.ru>
2018-12-26 20:34:16 +0300

    Update Domain.__init__

--- converter/converter/components/models/domain.py
+++ converter/converter/components/models/domain.py
@@ -1,19 +1,20 @@
 class Domain:
 
-    def __init__(self):
-        self.name = None
-        self.description = None
-        self.type = None
-        self.align = None
-        self.width = None
-        self.precision = None
+    def __init__(self, dictionary = {}):
+
+        self.name = dictionary.get("name")
+        self.description = dictionary.get("description")
+        self.type = dictionary.get("type")
+        self.align = dictionary.get("align")
+        self.width = dictionary.get("width")
+        self.precision = dictionary.get("precision")
         
         # Props
-        self.show_null = False
-        self.summable = False
-        self.case_sensitive = False
-        self.show_lead_nulls = False
-        self.thousands_separator = False
-        self.char_length = None
-        self.length = None
-        self.scale = None
+        self.show_null = dictionary.get("show_null", False)
+        self.summable = dictionary.get("summable", False)
+        self.case_sensitive = dictionary.get("case_sensitive", False)
+        self.show_lead_nulls = dictionary.get("show_lead_nulls", False)
+        self.thousands_separator = dictionary.get("thousands_separator", False)
+        self.char_length = dictionary.get("char_length")
+        self.length = dictionary.get("length")
+        self.scale = dictionary.get("scale")
